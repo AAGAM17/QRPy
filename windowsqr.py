5f99e1a97486078908ee6a8f49ca0ddb096aedb8
AAGAM SHAH <aagamcshah172005@gmai;.com>
2023-12-20 19:43:51 +0530

    Qr Code Generator using Python

---
+++ windowsqr.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+import pyqrcode
+from tkinter import filedialog
+
+def generate_qr_code():
+    url = url_entry.get()
+    qr = pyqrcode.create(url)
+    qr.png('temp.png', scale=10)
+    qr_img = tk.PhotoImage(file='temp.png')
+    qr_label.config(image=qr_img)
+    qr_label.image = qr_img
+    save_button.pack()
+
+def save_qr_code():
+    url = url_entry.get()
+    qr = pyqrcode.create(url)
+    file_path = filedialog.asksaveasfilename(defaultextension=".png")
+    qr.png(file_path, scale=10)
+
+window = tk.Tk()
+window.title("QR Code Generator")
+
+url_label = tk.Label(window, text="Website URL:")
+url_label.pack()
+url_entry = tk.Entry(window)
+url_entry.pack()
+
+generate_button = tk.Button(window, text="Generate QR Code", command=generate_qr_code)
+generate_button.pack()
+
+qr_label = tk.Label(window)
+qr_label.pack()
+
+save_button = tk.Button(window, text="Save QR Code", command=save_qr_code)
+
+window.mainloop()
